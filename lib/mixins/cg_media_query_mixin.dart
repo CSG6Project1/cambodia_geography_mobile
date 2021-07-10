64761ba6f99b404dc86f2b3b1f15a0db9ff7060d
theacheng <theacheng.g6@gmail.com>
2021-07-10 20:37:01 +0700

    rename to consistent mixin

--- lib/mixins/cg_media_query_mixin.dart
+++ lib/mixins/cg_media_query_mixin.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-mixin CgMediaQuery<T extends StatefulWidget> on State<T> {
+mixin CgMediaQueryMixin<T extends StatefulWidget> on State<T> {
   Size get screenSize => MediaQuery.of(context).size;
   EdgeInsets get viewInsets => MediaQuery.of(context).viewInsets;
   EdgeInsets get viewPadding => MediaQuery.of(context).viewPadding;
