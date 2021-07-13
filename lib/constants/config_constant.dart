62e66f023f448c4768c6e131f3273462bc808f60
Darot <64341213+darot-chen@users.noreply.github.com>
2021-07-13 15:35:18 +0700

    close #2 home screen design (#9)

--- lib/constants/config_constant.dart
+++ lib/constants/config_constant.dart
@@ -3,4 +3,175 @@ import 'package:flutter/material.dart';
 class ConfigConstant {
   ConfigConstant._internal();
   static const titleConstraints = const BoxConstraints(minWidth: 80);
+
+  /// ```
+  /// margin0 = 4.0
+  /// ```
+  static const double margin0 = 4.0;
+
+  /// ```
+  /// margin1 = 8.0
+  /// ```
+  static const double margin1 = 8.0;
+
+  /// ```
+  /// margin2 = 16.0
+  /// ```
+  static const double margin2 = 16.0;
+
+  /// ```
+  /// iconSize = 16.0
+  /// ```
+  static const double iconSize1 = 16.0;
+
+  /// ```
+  /// iconSize2 = 24.0
+  /// ```
+  static const double iconSize2 = 24.0;
+
+  /// ```
+  /// iconSize3 = 32.0
+  /// ```
+  static const double iconSize3 = 32.0;
+
+  /// ```
+  /// iconSize4 = 48.0
+  /// ```
+  static const double iconSize4 = 48.0;
+
+  /// ```
+  /// iconSize4 = 64.0
+  /// ```
+  static const double iconSize5 = 64.0;
+
+  /// ```
+  /// objectHeight1 = 48.0
+  /// ```
+  static const double objectHeight1 = 48.0;
+
+  /// ```
+  /// objectHeight2 = 56.0
+  /// ```
+  static const double objectHeight2 = 56.0;
+
+  /// ```
+  /// objectHeight3 = 64.0
+  /// ```
+  static const double objectHeight3 = 64.0;
+
+  /// ```
+  /// objectHeight4 = 72.0
+  /// ```
+  static const double objectHeight4 = 72.0;
+
+  /// ```
+  /// objectHeight5 = 96.0
+  /// ```
+  static const double objectHeight5 = 96.0;
+
+  /// ```
+  /// objectHeight6 = 120.0
+  /// ```
+  static const double objectHeight6 = 120.0;
+
+  /// ```
+  /// objectHeight7 = 240.0
+  /// ```
+  static const double objectHeight7 = 240.0;
+
+  /// ```
+  /// radius = 4.0;
+  /// ```
+  static const double radius1 = 6.0;
+
+  /// ```
+  /// radius1 = 10.0
+  /// ```
+  static const double radius2 = 10.0;
+
+  /// ```
+  /// circlarRadius1 = BorderRadius.circular(4.0)
+  /// ```
+  static final BorderRadius circlarRadius1 = BorderRadius.circular(6.0);
+
+  /// ```
+  /// circlarRadius2 = BorderRadius.circular(10.0)
+  /// ```
+  static final BorderRadius circlarRadius2 = BorderRadius.circular(10.0);
+
+  /// ```
+  /// circlarRadiusTop1 = BorderRadius.vertical(
+  ///   top: Radius.circular(4.0),
+  /// );
+  /// ```
+  static final BorderRadius circlarRadiusTop1 = BorderRadius.vertical(
+    top: Radius.circular(6.0),
+  );
+
+  /// ```
+  /// circlarRadiusTop2 = BorderRadius.vertical(
+  ///   top: Radius.circular(10.0),
+  /// );
+  /// ```
+  static final BorderRadius circlarRadiusTop2 = BorderRadius.vertical(
+    top: Radius.circular(10.0),
+  );
+
+  /// ```
+  /// circlarRadiusBottom1 = BorderRadius.vertical(
+  ///   top: Radius.circular(4.0),
+  /// );
+  /// ```
+  static final BorderRadius circlarRadiusBottom1 = BorderRadius.vertical(
+    top: Radius.circular(6.0),
+  );
+
+  /// ```
+  /// circlarRadiusBottom2 = BorderRadius.vertical(
+  ///   top: Radius.circular(10.0),
+  /// );
+  /// ```
+  static final BorderRadius circlarRadiusBottom2 = BorderRadius.vertical(
+    bottom: Radius.circular(10.0),
+  );
+
+  /// ```
+  /// fadeDuration = const Duration(milliseconds: 250)
+  /// ```
+  static const Duration fadeDuration = const Duration(milliseconds: 250);
+
+  /// ```
+  /// duration = const Duration(milliseconds: 350)
+  /// ```
+  static const Duration duration = const Duration(milliseconds: 350);
+
+  /// ```
+  /// layoutPadding = const EdgeInsets.symmetric(
+  ///   horizontal: margin2,
+  ///   vertical: margin1,
+  /// );
+  /// ```
+  static const EdgeInsets layoutPadding = const EdgeInsets.symmetric(
+    horizontal: margin2,
+    vertical: margin1,
+  );
+
+  /// ```
+  /// border = Border.all(
+  ///   width: 1,
+  ///   color: Color(0xFFCECECE),
+  /// );
+  /// ```
+  static final Border border = Border.all(
+    width: 1,
+    color: Color(0xFFCECECE),
+  );
+
+  static final List<BoxShadow> boxShadows0 = [
+    BoxShadow(
+      blurRadius: 10,
+      offset: Offset(1.0, 1.0),
+      color: Color(0xFF7C7C7C).withOpacity(0.17),
+    ),
+  ];
 }
