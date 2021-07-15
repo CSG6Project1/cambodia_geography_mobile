827e90cd13a34a93a19854ac07c1a4bddc82b62e
theacheng <theacheng.g6@gmail.com>
2021-07-15 20:55:39 +0700

    fix: menu animation not reverse on pop

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -131,6 +131,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin, 
                 context: context,
               ),
             );
+            animationController.reverse();
           },
         )
       ],
--- lib/screens/search/cg_search_delegate.dart
+++ lib/screens/search/cg_search_delegate.dart
@@ -86,7 +86,6 @@ class CgSearchDelegate extends SearchDelegate<String> {
         ),
         onPressed: () {
           Navigator.pop(context);
-          animationController.reverse();
         },
       );
     } else {
