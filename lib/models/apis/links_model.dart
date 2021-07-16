549cf0a3ba87c964850df8e9ef1a28dcae86bc17
theacheng <theacheng.g6@gmail.com>
2021-07-16 22:55:43 +0700

    fix: add missing prev field in links

--- lib/models/apis/links_model.dart
+++ lib/models/apis/links_model.dart
@@ -3,28 +3,32 @@ import 'package:cambodia_geography/helper/app_helper.dart';
 class LinksModel {
   LinksModel({
     this.self,
+    this.prev,
     this.next,
     this.first,
     this.last,
   });
 
   String? self;
+  String? prev;
   String? next;
   String? first;
   String? last;
 
   LinksModel getPageNumber() {
     return LinksModel(
+      self: AppHelper.queryParameters(url: this.self ?? "", param: 'page'),
+      prev: AppHelper.queryParameters(url: this.prev ?? "", param: 'page'),
+      next: AppHelper.queryParameters(url: this.next ?? "", param: 'page'),
       first: AppHelper.queryParameters(url: this.first ?? "", param: 'page'),
       last: AppHelper.queryParameters(url: this.last ?? "", param: 'page'),
-      next: AppHelper.queryParameters(url: this.next ?? "", param: 'page'),
-      self: AppHelper.queryParameters(url: this.self ?? "", param: 'page'),
     );
   }
 
   factory LinksModel.fromJson(Map<String, dynamic> json) {
     return LinksModel(
       self: json["self"],
+      prev: json["prev"],
       next: json["next"],
       first: json["first"],
       last: json["last"],
@@ -34,6 +38,7 @@ class LinksModel {
   Map<String, dynamic> toJson() {
     return {
       "self": self,
+      "prev": prev,
       "next": next,
       "first": first,
       "last": last,
