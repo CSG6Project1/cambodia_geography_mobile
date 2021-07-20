3cb66c750871e024d5c49d5d59c81392e41e24ea
theacheng <theacheng.g6@gmail.com>
2021-07-20 19:08:02 +0700

    close #34 social auth integration

--- lib/screens/user/user_screen.dart
+++ lib/screens/user/user_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cambodia_geography/app.dart';
 import 'package:cambodia_geography/exports/exports.dart';
+import 'package:cambodia_geography/services/authentications/social_auth_service.dart';
 import 'package:cambodia_geography/widgets/cg_app_bar_title.dart';
 import 'package:swipeable_page_route/swipeable_page_route.dart';
 
@@ -11,8 +12,31 @@ class UserScreen extends StatefulWidget {
 }
 
 class _UserScreenState extends State<UserScreen> {
+  late SocialAuthService socialAuthService;
+
+  @override
+  void initState() {
+    socialAuthService = SocialAuthService();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final socialTiles = [
+      _TileSetting(
+        title: "Facebook",
+        onPressed: () async {
+          await socialAuthService.getFacebookIdToken();
+        },
+      ),
+      _TileSetting(
+        title: "Google",
+        onPressed: () async {
+          await socialAuthService.getGoogleIdToken();
+        },
+      ),
+    ];
+
     return Scaffold(
       appBar: MorphingAppBar(
         title: CgAppBarTitle(title: AppLocalizations.of(context)!.helloWorld),
@@ -30,9 +54,23 @@ class _UserScreenState extends State<UserScreen> {
                 App.of(context)?.updateLocale(supportedLocales.first);
               }
             },
-          )
+          ),
+          for (var item in socialTiles)
+            ListTile(
+              title: Text(item.title),
+              onTap: item.onPressed,
+            )
         ],
       ),
     );
   }
+}
+
+class _TileSetting {
+  final String title;
+  final void Function() onPressed;
+  _TileSetting({
+    required this.title,
+    required this.onPressed,
+  });
 }
