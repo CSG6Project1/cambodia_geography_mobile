6c3de3bb1bfcf4342f7984c0b28e92b9d2d5f3ee
theacheng <theacheng.g6@gmail.com>
2021-07-20 21:15:36 +0700

    fix: add missing authorization in header

--- lib/services/networks/interceptors/user_token_interceptor.dart
+++ lib/services/networks/interceptors/user_token_interceptor.dart
@@ -1,11 +1,18 @@
+import 'package:cambodia_geography/models/apis/user_token_model.dart';
+import 'package:cambodia_geography/services/authentications/auth_api.dart';
 import 'package:http_interceptor/http/interceptor_contract.dart';
 import 'package:http_interceptor/http_interceptor.dart';
 
 class UserTokenInterceptor implements InterceptorContract {
+  AuthApi authApi = AuthApi();
+
   @override
   Future<RequestData> interceptRequest({required RequestData data}) async {
     try {
-      data.headers["Content-Type"] = "application/json";
+      UserTokenModel? model = await authApi.getCurrentUserToken();
+      if (model?.accessToken != null) {
+        data.headers["Authorization"] = "Bearer ${model?.accessToken}";
+      }
     } catch (e) {}
     return data;
   }
