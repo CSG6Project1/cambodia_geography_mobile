69a1a321635e863a1f888f0b7673c03b7cd48619
theacheng <theacheng.g6@gmail.com>
2021-07-26 10:17:49 +0700

    fix: add dispose to place detail

--- lib/screens/place_detail/place_detail_screen.dart
+++ lib/screens/place_detail/place_detail_screen.dart
@@ -2,6 +2,7 @@ import 'package:cambodia_geography/cambodia_geography.dart';
 import 'package:cambodia_geography/constants/config_constant.dart';
 import 'package:cambodia_geography/exports/widgets_exports.dart';
 import 'package:cambodia_geography/helper/number_helper.dart';
+import 'package:cambodia_geography/mixins/cg_media_query_mixin.dart';
 import 'package:cambodia_geography/mixins/cg_theme_mixin.dart';
 import 'package:cambodia_geography/models/places/place_model.dart';
 import 'package:cambodia_geography/screens/place_detail/local_widgets/place_title.dart';
@@ -22,30 +23,42 @@ class PlaceDetailScreen extends StatefulWidget {
   _PlaceDetailScreenState createState() => _PlaceDetailScreenState();
 }
 
-class _PlaceDetailScreenState extends State<PlaceDetailScreen> with CgThemeMixin {
+class _PlaceDetailScreenState extends State<PlaceDetailScreen> with CgThemeMixin, CgMediaQueryMixin {
   late ScrollController scrollController;
   late PageController pageController;
   late bool isShow;
   late CambodiaGeography geo;
+  late PlaceModel place;
 
   @override
   void initState() {
     scrollController = ScrollController();
     pageController = PageController();
     geo = CambodiaGeography.instance;
+    place = widget.place;
     isShow = false;
     super.initState();
+    scrollController.addListener(_listener);
+  }
+
+  @override
+  void dispose() {
+    scrollController.removeListener(_listener);
+    scrollController.dispose();
+    pageController.dispose();
+    super.dispose();
+  }
+
+  void _listener() {
+    if (scrollController.offset >= mediaQueryData.size.width / 2) {
+      setState(() => isShow = true);
+    } else {
+      setState(() => isShow = false);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    PlaceModel place = widget.place;
-    scrollController.addListener(() {
-      if (scrollController.offset >= MediaQuery.of(context).size.width / 2)
-        setState(() => isShow = true);
-      else
-        setState(() => isShow = false);
-    });
     return Scaffold(
       body: CustomScrollView(
         controller: scrollController,
@@ -61,7 +74,7 @@ class _PlaceDetailScreenState extends State<PlaceDetailScreen> with CgThemeMixin
   Container buildBottomNav(PlaceModel place) {
     return Container(
       height: ConfigConstant.objectHeight4,
-      padding: EdgeInsets.only(
+      padding: const EdgeInsets.only(
         bottom: ConfigConstant.margin2,
         left: 18,
         right: 18,
@@ -71,7 +84,7 @@ class _PlaceDetailScreenState extends State<PlaceDetailScreen> with CgThemeMixin
         boxShadow: [
           BoxShadow(
             color: Colors.grey,
-            offset: Offset(0, -2),
+            offset: const Offset(0, -2),
             blurRadius: 8,
           ),
         ],
@@ -91,7 +104,7 @@ class _PlaceDetailScreenState extends State<PlaceDetailScreen> with CgThemeMixin
             NumberHelper.toKhmer((place.commentLength ?? 0).toString()),
             style: textTheme.caption,
           ),
-          Expanded(child: SizedBox()),
+          const Expanded(child: SizedBox()),
           IconButton(
             onPressed: () {},
             icon: Icon(
@@ -117,22 +130,28 @@ class _PlaceDetailScreenState extends State<PlaceDetailScreen> with CgThemeMixin
         [
           PlaceTitle(place: place),
           Container(
-            margin: EdgeInsets.symmetric(vertical: ConfigConstant.margin2),
-            padding: EdgeInsets.all(ConfigConstant.margin2),
+            margin: const EdgeInsets.symmetric(vertical: ConfigConstant.margin2),
+            padding: const EdgeInsets.all(ConfigConstant.margin2),
             color: colorScheme.surface,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
                   '${place.khmer}',
-                  style: textTheme.headline6?.copyWith(fontWeight: FontWeight.w700, color: Colors.grey),
+                  style: textTheme.headline6?.copyWith(
+                    fontWeight: FontWeight.w700,
+                    color: colorScheme.onSurface,
+                  ),
                 ),
                 MarkdownBody(
                   data: place.body.toString(),
                   styleSheet: MarkdownStyleSheet.fromTheme(
                     ThemeData(
                       textTheme: TextTheme(
-                        bodyText2: TextStyle(fontSize: 12.0, color: Colors.grey),
+                        bodyText2: TextStyle(
+                          fontSize: 12.0,
+                          color: colorScheme.onSurface,
+                        ),
                       ),
                     ),
                   ),
@@ -145,35 +164,37 @@ class _PlaceDetailScreenState extends State<PlaceDetailScreen> with CgThemeMixin
     );
   }
 
+  Future<void> onViewImages(List<String> images) async {
+    return showModalBottomSheet(
+      context: context,
+      useRootNavigator: true,
+      isScrollControlled: true,
+      backgroundColor: Colors.black,
+      barrierColor: Colors.transparent,
+      builder: (context) {
+        return ImageViewer(
+          images: images,
+          statusBarHeight: MediaQuery.of(context).padding.top,
+          currentImageIndex: pageController.page!.toInt(),
+          onPageChanged: (index) {
+            pageController.animateToPage(
+              index,
+              duration: ConfigConstant.duration,
+              curve: Curves.easeIn,
+            );
+          },
+        );
+      },
+    );
+  }
+
   SliverAppBar buildAppBar({
     required BuildContext context,
     required PlaceModel place,
   }) {
     final double expandedHeight = MediaQuery.of(context).size.width / 2;
-    var onViewImage = () {
-      showModalBottomSheet(
-        context: context,
-        useRootNavigator: true,
-        isScrollControlled: true,
-        backgroundColor: Colors.black,
-        barrierColor: Colors.transparent,
-        builder: (context) {
-          List<String> images = place.images!.map((e) => e.url ?? '').toList();
-          return ImageViewer(
-            images: images,
-            statusBarHeight: MediaQuery.of(context).padding.top,
-            currentImageIndex: pageController.page!.toInt(),
-            onPageChanged: (index) {
-              pageController.animateToPage(
-                index,
-                duration: ConfigConstant.duration,
-                curve: Curves.easeIn,
-              );
-            },
-          );
-        },
-      );
-    };
+    List<String> images = place.images?.map((e) => e.url ?? '').toList() ?? [];
+
     return SliverAppBar(
       elevation: 0,
       brightness: isShow ? Brightness.dark : Brightness.light,
@@ -190,7 +211,7 @@ class _PlaceDetailScreenState extends State<PlaceDetailScreen> with CgThemeMixin
           color: isShow ? Colors.transparent : Colors.black26,
         ),
         child: IconButton(
-          icon: Icon(Icons.arrow_back),
+          icon: const Icon(Icons.arrow_back),
           onPressed: () => Navigator.pop(context),
         ),
       ),
@@ -199,7 +220,9 @@ class _PlaceDetailScreenState extends State<PlaceDetailScreen> with CgThemeMixin
           title: isShow ? place.khmer.toString() : '',
         ),
         background: GestureDetector(
-          onTap: onViewImage,
+          onTap: () {
+            if (images.isNotEmpty) onViewImages(images);
+          },
           child: PageIndicatorContainer(
             padding: const EdgeInsets.only(bottom: ConfigConstant.margin2),
             indicatorColor: Theme.of(context).colorScheme.surface,
@@ -207,7 +230,7 @@ class _PlaceDetailScreenState extends State<PlaceDetailScreen> with CgThemeMixin
             length: place.images?.length ?? 0,
             shape: IndicatorShape.roundRectangleShape(
               size: const Size(8, 8),
-              cornerSize: Size.square(4),
+              cornerSize: const Size.square(4),
             ),
             child: PageView(
               controller: pageController,
