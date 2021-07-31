d06db5d5cab4f66aa7740a8f318c0aa64d9d91fa
VatanakChamroeun <ch.vatanak01@gmail.com>
2021-07-31 11:53:44 +0700

    close #52 search history

---
+++ lib/screens/search/search_history_storage.dart
@@ -0,0 +1,8 @@
+import 'package:cambodia_geography/services/base_storages/list_preference_storage.dart';
+
+class SearchHistoryStorage extends ListPreferenceStorage {
+  @override
+  String get key => "SearchHistoryStorage";
+}
+
+
