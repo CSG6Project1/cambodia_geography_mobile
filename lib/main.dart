ee983aa8128889e1b673690582e8e55dc28ab2a6
Thea Choem <theacheng.g6@gmail.com>
2021-08-02 13:31:20 +0700

    feat: add circle splash screen (#60)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:cambodia_geography/app.dart';
 import 'package:cambodia_geography/cambodia_geography.dart';
+import 'package:cambodia_geography/constants/theme_constant.dart';
 import 'package:cambodia_geography/services/storages/locale_storage.dart';
 import 'package:cambodia_geography/services/storages/theme_mode_storage.dart';
 import 'package:flutter/material.dart';
@@ -8,28 +9,67 @@ import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp();
-  await CambodiaGeography.instance.initilize();
-
-  bool isDarkMode = await getInitialDarkMode();
-  Locale? locale = await getInitialLocale();
-
   runApp(
-    App(
-      initialIsDarkMode: isDarkMode,
-      initialLocale: locale,
+    MaterialApp(
+      home: SplashScreen(),
     ),
   );
 }
 
-Future<Locale?> getInitialLocale() async {
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _initializeApp().then(_goToNextPage);
+  }
+
+  void _goToNextPage(_IntModel value) {
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(
+        builder: (_) => App(
+          initialIsDarkMode: value.isDarkMode == true,
+          initialLocale: value.locale,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(
+          color: ThemeConstant.lightScheme.primary,
+        ),
+      ),
+    );
+  }
+}
+
+Future<_IntModel> _initializeApp() async {
+  await Firebase.initializeApp();
+  await CambodiaGeography.instance.initilize();
+
+  bool isDarkMode = await _getInitialDarkMode();
+  Locale? locale = await _getInitialLocale();
+
+  return _IntModel(isDarkMode, locale);
+}
+
+Future<Locale?> _getInitialLocale() async {
   LocaleStorage storage = LocaleStorage();
   Locale? locale = await storage.readLocale();
   return locale;
 }
 
-Future<bool> getInitialDarkMode() async {
+Future<bool> _getInitialDarkMode() async {
   ThemeModeStorage storage = ThemeModeStorage();
   bool? isDarkMode = await storage.readBool();
   if (isDarkMode == null) {
@@ -37,4 +77,10 @@ Future<bool> getInitialDarkMode() async {
     isDarkMode = platformBrightness == Brightness.dark;
   }
   return isDarkMode;
+}
+
+class _IntModel {
+  final bool? isDarkMode;
+  final Locale? locale;
+  _IntModel(this.isDarkMode, this.locale);
 }
