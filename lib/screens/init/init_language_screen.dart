3649e602c7b03406fd9c8759060d6e36952e09d3
theacheng <theacheng.g6@gmail.com>
2021-08-06 15:24:00 +0700

    feat: add initial language screen

---
+++ lib/screens/init/init_language_screen.dart
@@ -0,0 +1,97 @@
+import 'package:cambodia_geography/app.dart';
+import 'package:cambodia_geography/configs/route_config.dart';
+import 'package:cambodia_geography/constants/app_constant.dart';
+import 'package:cambodia_geography/constants/config_constant.dart';
+import 'package:cambodia_geography/exports/widgets_exports.dart';
+import 'package:cambodia_geography/mixins/cg_media_query_mixin.dart';
+import 'package:cambodia_geography/mixins/cg_theme_mixin.dart';
+import 'package:cambodia_geography/services/storages/init_app_state_storage.dart';
+import 'package:cambodia_geography/services/storages/locale_storage.dart';
+import 'package:cambodia_geography/types/app_state_type.dart';
+import 'package:flutter/material.dart';
+
+class InitLanguageScreen extends StatefulWidget {
+  const InitLanguageScreen({Key? key}) : super(key: key);
+
+  @override
+  _InitLanguageScreenState createState() => _InitLanguageScreenState();
+}
+
+class _InitLanguageScreenState extends State<InitLanguageScreen> with CgMediaQueryMixin, CgThemeMixin {
+  late InitAppStateStorage appStateStorage;
+  late LocaleStorage localeStorage;
+
+  @override
+  void initState() {
+    appStateStorage = InitAppStateStorage();
+    localeStorage = LocaleStorage();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: buildPrimaryTheme(),
+      child: Scaffold(
+        appBar: AppBar(),
+        backgroundColor: colorScheme.primary,
+        body: Padding(
+          padding: ConfigConstant.layoutPadding,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CgAppBarTitle(
+                title: "ជ្រើសរើសភាសា",
+                textStyle: textTheme.headline6?.copyWith(
+                  fontWeight: FontWeight.w600,
+                  color: colorScheme.onPrimary,
+                ),
+              ),
+              Text(
+                "សូមជ្រើសយកមួយភាសា",
+              ),
+              const SizedBox(height: ConfigConstant.margin2),
+              Container(
+                width: double.infinity,
+                child: CgButton(
+                  labelText: "ខ្មែរ",
+                  backgroundColor: colorScheme.primaryVariant,
+                  onPressed: () async {
+                    onSubmit(AppContant.km);
+                  },
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                child: CgButton(
+                  labelText: "English",
+                  backgroundColor: colorScheme.primaryVariant,
+                  onPressed: () async {
+                    onSubmit(AppContant.en);
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> onSubmit(Locale locale) async {
+    await App.of(context)?.updateLocale(locale);
+    appStateStorage.setCurrentState(AppStateType.setLangauge);
+    Navigator.of(context).pushReplacementNamed(RouteConfig.LOGIN);
+  }
+
+  ThemeData buildPrimaryTheme() {
+    return themeData.copyWith(
+      textTheme: textTheme.apply(
+        bodyColor: colorScheme.onPrimary,
+        displayColor: colorScheme.onPrimary,
+        decorationColor: colorScheme.onPrimary,
+      ),
+    );
+  }
+}
