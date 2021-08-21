91ebc224b7aa4c47c28710f8369c62f55bc5a39a
theacheng <theacheng.g6@gmail.com>
2021-08-21 20:31:33 +0700

    feat: add no data widget

--- lib/screens/admin/local_widgets/place_list.dart
+++ lib/screens/admin/local_widgets/place_list.dart
@@ -1,4 +1,5 @@
 import 'package:cambodia_geography/constants/config_constant.dart';
+import 'package:cambodia_geography/mixins/cg_theme_mixin.dart';
 import 'package:cambodia_geography/models/places/place_list_model.dart';
 import 'package:cambodia_geography/models/places/place_model.dart';
 import 'package:cambodia_geography/screens/places/local_widgets/place_card.dart';
@@ -27,8 +28,10 @@ class PlaceList extends StatefulWidget {
   _PlaceListState createState() => _PlaceListState();
 }
 
-class _PlaceListState extends State<PlaceList> with AutomaticKeepAliveClientMixin {
+class _PlaceListState extends State<PlaceList> with AutomaticKeepAliveClientMixin, CgThemeMixin {
   late PlacesApi placesApi;
+  late bool loading;
+
   String? provinceCode;
   PlaceListModel? placeList;
 
@@ -37,6 +40,7 @@ class _PlaceListState extends State<PlaceList> with AutomaticKeepAliveClientMixi
     placesApi = PlacesApi();
     provinceCode = widget.provinceCode;
     super.initState();
+    loading = true;
     if (provinceCode != null) load();
   }
 
@@ -51,6 +55,7 @@ class _PlaceListState extends State<PlaceList> with AutomaticKeepAliveClientMixi
 
     if (placesApi.success() && result != null) {
       setState(() {
+        loading = false;
         if (placeList != null) {
           placeList?.add(result);
         } else {
@@ -64,33 +69,75 @@ class _PlaceListState extends State<PlaceList> with AutomaticKeepAliveClientMixi
   Widget build(BuildContext context) {
     super.build(context);
     List<PlaceModel>? places = placeList?.items;
-    return CgLoadMoreList(
-      onEndScroll: () => load(loadMore: true),
-      child: ListView.separated(
-        padding: ConfigConstant.layoutPadding,
-        itemCount: places == null ? 5 : places.length + 1,
-        separatorBuilder: (context, index) {
-          return SizedBox(height: ConfigConstant.margin1);
-        },
-        itemBuilder: (context, index) {
-          if (places?.length == index) {
-            return Visibility(
-              visible: placeList?.hasLoadMore() == true,
-              child: Container(
-                alignment: Alignment.center,
-                padding: ConfigConstant.layoutPadding,
-                child: const CircularProgressIndicator(),
-              ),
-            );
-          }
-          return PlaceCard(
-            place: places?[index],
-            onTap: () {
-              if (places?[index] == null) return;
-              widget.onTap(places![index]);
+
+    return Stack(
+      children: [
+        CgLoadMoreList(
+          onEndScroll: () => load(loadMore: true),
+          child: ListView.separated(
+            padding: ConfigConstant.layoutPadding,
+            itemCount: places == null ? 5 : places.length + 1,
+            separatorBuilder: (context, index) {
+              return const SizedBox(
+                height: ConfigConstant.margin1,
+              );
             },
-          );
-        },
+            itemBuilder: (context, index) {
+              if (places?.length == index) return buildLoadMoreLoading();
+              return PlaceCard(
+                place: places?[index],
+                onTap: () {
+                  if (places?[index] == null) return;
+                  widget.onTap(places![index]);
+                },
+              );
+            },
+          ),
+        ),
+        buildNoData(places)
+      ],
+    );
+  }
+
+  Visibility buildLoadMoreLoading() {
+    return Visibility(
+      visible: placeList?.hasLoadMore() == true,
+      child: Container(
+        alignment: Alignment.center,
+        padding: ConfigConstant.layoutPadding,
+        child: const CircularProgressIndicator(),
+      ),
+    );
+  }
+
+  Widget buildNoData(List<PlaceModel>? places) {
+    return Positioned(
+      top: 0,
+      bottom: 0,
+      right: 0,
+      left: 0,
+      child: IgnorePointer(
+        ignoring: !loading && places != null && places.isEmpty ? false : true,
+        child: AnimatedOpacity(
+          duration: ConfigConstant.duration,
+          opacity: !loading && places != null && places.isEmpty ? 1 : 0,
+          child: Container(
+            color: colorScheme.surface,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.map,
+                  size: ConfigConstant.iconSize5,
+                  color: colorScheme.primary,
+                ),
+                const SizedBox(height: ConfigConstant.margin1),
+                Text("មិនមានទិន្នន័យ"),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
