0f171cc3c71e410b9c2babc2cc12d1160499bc6d
theacheng <theacheng.g6@gmail.com>
2021-08-22 13:50:14 +0700

    refactor: improve not image widget

--- lib/widgets/cg_network_image_loader.dart
+++ lib/widgets/cg_network_image_loader.dart
@@ -1,4 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cambodia_geography/constants/config_constant.dart';
 import 'package:cambodia_geography/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
 
@@ -9,24 +10,42 @@ class CgNetworkImageLoader extends StatelessWidget {
     this.width,
     this.height,
     this.fit,
+    this.showError = true,
   }) : super(key: key);
 
   final String? imageUrl;
   final double? width;
   final double? height;
   final BoxFit? fit;
+  final bool showError;
 
   @override
   Widget build(BuildContext context) {
-    if (imageUrl == null) return Image.asset(Assets.images.helperImage.placeholder.path);
+    if (imageUrl == null) return buildNoImage();
     return CachedNetworkImage(
       imageUrl: imageUrl!,
       width: width,
       height: height,
       fit: fit,
       errorWidget: (context, url, error) {
-        return Image.asset(Assets.images.helperImage.placeholder.path);
+        return buildNoImage();
       },
+    );
+  }
+
+  Widget buildNoImage() {
+    return Container(
+      width: width,
+      height: height,
+      padding: const EdgeInsets.all(ConfigConstant.margin2),
+      child: showError
+          ? Image.asset(
+              Assets.images.helperImage.placeholder.path,
+              width: width,
+              height: height,
+              fit: fit,
+            )
+          : null,
     );
   }
 }
