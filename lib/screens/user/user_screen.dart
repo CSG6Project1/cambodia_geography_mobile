87177a7136e7e2d0881e1e71003fe39d4bbefbdb
Thea Choem <theacheng.g6@gmail.com>
2021-08-22 16:30:29 +0700

    close #70 user screen design (#71)

--- lib/screens/user/user_screen.dart
+++ lib/screens/user/user_screen.dart
@@ -1,10 +1,22 @@
+import 'dart:io';
+import 'dart:ui';
+
+import 'package:adaptive_dialog/adaptive_dialog.dart';
+import 'package:cambodia_geography/configs/route_config.dart';
 import 'package:cambodia_geography/constants/config_constant.dart';
 import 'package:cambodia_geography/exports/exports.dart';
+import 'package:cambodia_geography/mixins/cg_media_query_mixin.dart';
+import 'package:cambodia_geography/mixins/cg_theme_mixin.dart';
+import 'package:cambodia_geography/providers/locale_provider.dart';
+import 'package:cambodia_geography/providers/theme_provider.dart';
 import 'package:cambodia_geography/providers/user_provider.dart';
 import 'package:cambodia_geography/services/authentications/social_auth_service.dart';
+import 'package:cambodia_geography/services/images/image_picker_service.dart';
 import 'package:cambodia_geography/widgets/cg_app_bar_title.dart';
+import 'package:cambodia_geography/widgets/cg_network_image_loader.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:image_cropper/image_cropper.dart';
 import 'package:provider/provider.dart';
-import 'package:swipeable_page_route/swipeable_page_route.dart';
 
 class UserScreen extends StatefulWidget {
   const UserScreen({Key? key}) : super(key: key);
@@ -13,70 +25,400 @@ class UserScreen extends StatefulWidget {
   _UserScreenState createState() => _UserScreenState();
 }
 
-class _UserScreenState extends State<UserScreen> {
+class _UserScreenState extends State<UserScreen> with CgMediaQueryMixin, CgThemeMixin, SingleTickerProviderStateMixin {
   late SocialAuthService socialAuthService;
+  late AnimationController controller;
 
   @override
   void initState() {
     socialAuthService = SocialAuthService();
+    controller = AnimationController(vsync: this, duration: ConfigConstant.fadeDuration);
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  Future<void> showThemeModeDialog(ThemeProvider provider) async {
+    var key = await showConfirmationDialog(
+      context: context,
+      title: "Theme",
+      initialSelectedActionKey: provider.isDarkMode ? "dark" : "light",
+      actions: [
+        AlertDialogAction(key: "system", label: "System"),
+        AlertDialogAction(key: "dark", label: "Dark Mode"),
+        AlertDialogAction(key: "light", label: "Light Mode"),
+      ],
+    );
+    switch (key) {
+      case "dark":
+        provider.turnDarkModeOn();
+        break;
+      case "light":
+        provider.turnDarkModeOff();
+        break;
+      default:
+    }
+  }
+
+  Future<void> showLanguageDialog(LocaleProvider provider) async {
+    var key = await showConfirmationDialog(
+      context: context,
+      title: "Language",
+      initialSelectedActionKey: "en",
+      actions: [
+        // AlertDialogAction(key: "system", label: "System"),
+        AlertDialogAction(key: "en", label: "English"),
+        AlertDialogAction(key: "km", label: "Khmer"),
+      ],
+    );
+    switch (key) {
+      case "en":
+        provider.updateLocale(Locale("en"));
+        break;
+      case "km":
+        provider.updateLocale(Locale("en"));
+        break;
+      case "system":
+        provider.useDefaultLocale();
+        break;
+      default:
+    }
+  }
+
+  Future<void> showUpdateNameDialog(UserProvider provider) async {
+    List<String>? values = await showTextInputDialog(
+      context: context,
+      title: "Update name",
+      textFields: [
+        DialogTextField(
+          hintText: "New name",
+          initialText: provider.user?.username,
+        ),
+      ],
+    );
+    if (values?.isNotEmpty == true) {
+      print(values?.first);
+      // provider.fetchCurrentUser();
+    }
+  }
+
+  Future<void> showUpdatePasswordDialog(UserProvider provider) async {
+    List<String>? values = await showTextInputDialog(
+      context: context,
+      title: "Change password",
+      textFields: [
+        DialogTextField(
+          hintText: "Password",
+          obscureText: true,
+        ),
+        DialogTextField(
+          hintText: "Confirm Password",
+          obscureText: true,
+        ),
+      ],
+    );
+    if (values?.isNotEmpty == true) {
+      print(values?.first);
+      // provider.fetchCurrentUser();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     var provider = Provider.of<UserProvider>(context, listen: true);
-    final socialTiles = [
-      _TileSetting(
-        title: "Facebook",
-        onPressed: () async {
-          await socialAuthService.getFacebookIdToken();
-        },
-      ),
-      _TileSetting(
-        title: "Google",
-        onPressed: () async {
-          await socialAuthService.getGoogleIdToken();
-        },
-      ),
-    ];
+
+    if (provider.user != null) {
+      if (controller.isDismissed) controller.forward();
+    } else {
+      if (controller.isCompleted) controller.reverse();
+    }
 
     return Scaffold(
-      appBar: MorphingAppBar(
-        title: CgAppBarTitle(title: "User screen"),
-      ),
-      body: ListView(
-        children: [
-          ListTile(
-            title: Text("Language"),
-            subtitle: Text("En"),
-            onTap: () {
-              // TODO: change localize here
-              // final supportedLocales = AppLocalizations.supportedLocales;
-              // if (supportedLocales.first.languageCode == AppLocalizations.of(context)?.localeName) {
-              //   App.of(context)?.updateLocale(supportedLocales.last);
-              // } else {
-              //   App.of(context)?.updateLocale(supportedLocales.first);
-              // }
-            },
-          ),
-          for (var item in socialTiles)
-            ListTile(
-              title: Text(item.title),
-              onTap: item.onPressed,
-            ),
-          AnimatedCrossFade(
-            duration: ConfigConstant.fadeDuration,
-            crossFadeState: provider.isSignedIn ? CrossFadeState.showFirst : CrossFadeState.showSecond,
-            secondChild: SizedBox(),
-            firstChild: ListTile(
-              title: Text("Sign Out"),
-              onTap: () {
-                provider.signOut();
-              },
+      body: CustomScrollView(
+        slivers: [
+          buildAppBar(provider),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                AnimatedCrossFade(
+                  duration: ConfigConstant.fadeDuration,
+                  crossFadeState: provider.isSignedIn ? CrossFadeState.showSecond : CrossFadeState.showFirst,
+                  secondChild: Column(
+                    children: [
+                      buildSettingTile(
+                        title: "Name",
+                        subtitle: provider.user?.username,
+                        iconData: Icons.person,
+                        onTap: () {
+                          showUpdateNameDialog(provider);
+                        },
+                      ),
+                      buildSettingTile(
+                        title: "Email",
+                        iconData: Icons.mail,
+                        subtitle: provider.user?.email,
+                        onTap: () {},
+                      ),
+                      buildSettingTile(
+                        title: "Password",
+                        iconData: Icons.remove_red_eye,
+                        onTap: () => showUpdatePasswordDialog(provider),
+                      ),
+                      buildSettingTile(
+                        title: "Log Out",
+                        iconData: Icons.logout,
+                        showDivider: false,
+                        onTap: () {
+                          provider.signOut();
+                        },
+                      ),
+                    ],
+                  ),
+                  firstChild: buildSettingTile(
+                    title: "Login",
+                    iconData: Icons.login,
+                    showDivider: false,
+                    onTap: () {
+                      Navigator.of(context).pushNamed(RouteConfig.LOGIN);
+                    },
+                  ),
+                ),
+                const SizedBox(height: ConfigConstant.margin2),
+                Consumer<ThemeProvider>(
+                  builder: (context, provider, child) {
+                    return AnimatedCrossFade(
+                      duration: ConfigConstant.fadeDuration,
+                      crossFadeState: provider.isDarkMode ? CrossFadeState.showSecond : CrossFadeState.showFirst,
+                      secondChild: buildSettingTile(
+                        title: "Theme",
+                        iconData: Icons.dark_mode,
+                        subtitle: "Dark Mode",
+                        onTap: () => showThemeModeDialog(provider),
+                      ),
+                      firstChild: buildSettingTile(
+                        title: "Theme",
+                        iconData: Icons.light_mode,
+                        subtitle: "Light Mode",
+                        onTap: () => showThemeModeDialog(provider),
+                      ),
+                    );
+                  },
+                ),
+                Consumer<LocaleProvider>(
+                  builder: (context, provider, child) {
+                    return buildSettingTile(
+                      title: "Language",
+                      iconData: Icons.language,
+                      subtitle: "English",
+                      showDivider: false,
+                      onTap: () => showLanguageDialog(provider),
+                    );
+                  },
+                ),
+                const SizedBox(height: ConfigConstant.margin2),
+                buildSettingTile(
+                  title: "Rate our app",
+                  iconData: Icons.rate_review,
+                  subtitle: "We’d love to hear your experience",
+                  onTap: () {},
+                ),
+                buildSettingTile(
+                  title: "Policy & Privary",
+                  iconData: Icons.privacy_tip,
+                  showDivider: false,
+                  onTap: () {},
+                ),
+                const SizedBox(height: ConfigConstant.objectHeight7),
+              ],
             ),
           )
         ],
       ),
+    );
+  }
+
+  Material buildSettingTile({
+    required String title,
+    required IconData iconData,
+    String? subtitle,
+    void Function()? onTap,
+    bool showDivider = true,
+  }) {
+    return Material(
+      child: Column(
+        children: [
+          ListTile(
+            onTap: onTap,
+            leading: AspectRatio(
+              aspectRatio: 1,
+              child: Icon(
+                iconData,
+                color: colorScheme.primary,
+              ),
+            ),
+            title: Text(title),
+            subtitle: subtitle != null ? Text(subtitle) : null,
+          ),
+          if (showDivider) const Divider(height: 0, indent: ConfigConstant.objectHeight4)
+        ],
+      ),
+    );
+  }
+
+  Widget buildAppBar(UserProvider provider) {
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (context, snapshot) {
+        double value = controller.drive(CurveTween(curve: Curves.ease)).value;
+        return SliverAppBar(
+          pinned: true,
+          elevation: 0.0,
+          stretch: true,
+          expandedHeight: lerpDouble(kToolbarHeight, 2 * mediaQueryData.size.width / 3, value),
+          flexibleSpace: FlexibleSpaceBar(
+            background: Stack(
+              children: [
+                if (provider.user != null)
+                  Positioned.fill(
+                    child: AspectRatio(
+                      aspectRatio: 3 / 2,
+                      child: ImageFiltered(
+                        imageFilter: ImageFilter.blur(
+                          sigmaX: lerpDouble(10, 20, value) ?? 20,
+                          sigmaY: lerpDouble(10, 20, value) ?? 20,
+                          tileMode: TileMode.mirror,
+                        ),
+                        child: CgNetworkImageLoader(
+                          fit: BoxFit.cover,
+                          imageUrl: provider.user?.profileImg?.url,
+                        ),
+                      ),
+                    ),
+                  ),
+                if (provider.user != null)
+                  Positioned.fill(
+                    child: Wrap(
+                      alignment: WrapAlignment.center,
+                      crossAxisAlignment: WrapCrossAlignment.center,
+                      runAlignment: WrapAlignment.center,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const SizedBox(height: ConfigConstant.objectHeight1),
+                            Stack(
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(ConfigConstant.objectHeight5),
+                                    border: Border.all(color: Colors.white, width: 3),
+                                  ),
+                                  child: CgNetworkImageLoader(
+                                    imageUrl: provider.user?.profileImg?.url ?? "",
+                                    width: ConfigConstant.objectHeight5,
+                                    height: ConfigConstant.objectHeight5,
+                                    fit: BoxFit.cover,
+                                    borderRadius: BorderRadius.circular(ConfigConstant.objectHeight5),
+                                  ),
+                                ),
+                                Positioned.fill(
+                                  child: Material(
+                                    color: Colors.transparent,
+                                    child: InkWell(
+                                      borderRadius: BorderRadius.circular(ConfigConstant.objectHeight5),
+                                      onTap: () async {
+                                        File? file = await ImagePickerService.pickImage(
+                                          aspectRatio: CropAspectRatio(
+                                            ratioX: 1,
+                                            ratioY: 1,
+                                          ),
+                                        );
+                                        if (file != null) {
+                                          print(file.path);
+                                        }
+                                      },
+                                      child: Container(
+                                        width: ConfigConstant.objectHeight5,
+                                        height: ConfigConstant.objectHeight5,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Positioned(
+                                  bottom: 0,
+                                  right: 0,
+                                  child: Container(
+                                    width: ConfigConstant.iconSize3,
+                                    height: ConfigConstant.iconSize3,
+                                    decoration: BoxDecoration(
+                                      color: colorScheme.onPrimary,
+                                      borderRadius: BorderRadius.circular(ConfigConstant.iconSize3),
+                                    ),
+                                    child: IconButton(
+                                      icon: Icon(
+                                        Icons.image,
+                                        size: ConfigConstant.iconSize1,
+                                        color: colorScheme.primary,
+                                      ),
+                                      onPressed: () {},
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                            const SizedBox(height: ConfigConstant.margin2),
+                            Wrap(
+                              crossAxisAlignment: WrapCrossAlignment.center,
+                              alignment: WrapAlignment.center,
+                              children: [
+                                Text(
+                                  provider.user?.username ?? "",
+                                  style: themeData.appBarTheme.titleTextStyle,
+                                ),
+                                const SizedBox(width: ConfigConstant.margin0),
+                                AnimatedCrossFade(
+                                  crossFadeState: provider.user?.role == "admin"
+                                      ? CrossFadeState.showFirst
+                                      : CrossFadeState.showSecond,
+                                  duration: ConfigConstant.fadeDuration,
+                                  secondChild: SizedBox(width: double.infinity),
+                                  firstChild: Container(
+                                    padding: const EdgeInsets.symmetric(horizontal: ConfigConstant.margin1),
+                                    decoration: BoxDecoration(
+                                      borderRadius: ConfigConstant.circlarRadius2,
+                                      color: colorScheme.primary,
+                                    ),
+                                    child: Text(
+                                      provider.user?.role ?? "",
+                                      style: TextStyle(
+                                        color: colorScheme.onPrimary,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Text(
+                              provider.user?.email ?? "",
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
--- lib/services/images/image_picker_service.dart
+++ lib/services/images/image_picker_service.dart
@@ -5,21 +5,22 @@ import 'package:image_picker/image_picker.dart';
 class ImagePickerService {
   ImagePickerService._internal();
 
-  static Future<File?> pickImage() async {
+  static Future<File?> pickImage({CropAspectRatio? aspectRatio}) async {
     ImagePicker picker = ImagePicker();
     XFile? pickedFile = await picker.pickImage(source: ImageSource.gallery);
     print(pickedFile);
     if (pickedFile != null) {
       File file = File(pickedFile.path);
-      return cropAnImage(file);
+      return cropAnImage(file, aspectRatio);
     } else {
       print('No image selected.');
     }
   }
 
-  static Future<File?> cropAnImage(File imageFile) async {
+  static Future<File?> cropAnImage(File imageFile, CropAspectRatio? aspectRatio) async {
     File? croppedFile = await ImageCropper.cropImage(
       sourcePath: imageFile.path,
+      aspectRatio: aspectRatio,
       androidUiSettings: AndroidUiSettings(toolbarTitle: 'Cropper'),
       iosUiSettings: IOSUiSettings(),
     );
