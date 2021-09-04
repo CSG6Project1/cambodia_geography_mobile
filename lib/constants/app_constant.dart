bb8ab0dc61b34d4e04a5ef320ba1f83811422eda
theacheng <theacheng.g6@gmail.com>
2021-09-04 15:58:01 +0700

    close #114 add place type on admin

--- lib/constants/app_constant.dart
+++ lib/constants/app_constant.dart
@@ -1,11 +1,14 @@
 import 'package:cambodia_geography/exports/exports.dart';
+import 'package:cambodia_geography/screens/admin/local_widgets/place_list.dart';
 
 class AppContant {
   AppContant._internal();
-  static const List<String> placeType = [
-    "place",
-    "restaurant",
-  ];
+
+  static List<String> get placeType {
+    return PlaceType.values.map((e) {
+      return "$e".replaceAll("PlaceType.", "");
+    }).toList();
+  }
 
   static const Locale km = Locale("km");
   static const Locale en = Locale("en");
