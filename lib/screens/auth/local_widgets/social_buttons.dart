f2ac81a6fd4c0a1f1e322666141a211598658e74
Thea Choem <theacheng.g6@gmail.com>
2021-09-26 16:22:01 +0700

    close #168 login flow translation (#169)

--- lib/screens/auth/local_widgets/social_buttons.dart
+++ lib/screens/auth/local_widgets/social_buttons.dart
@@ -1,6 +1,7 @@
 import 'package:cambodia_geography/constants/config_constant.dart';
 import 'package:cambodia_geography/exports/exports.dart';
 import 'package:cambodia_geography/services/authentications/social_auth_service.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 enum SocialProviderType {
@@ -12,12 +13,15 @@ class SocialButtons extends StatelessWidget {
   const SocialButtons({
     Key? key,
     required this.onFetched,
+    required this.isSignUp,
   }) : super(key: key);
 
   final void Function(
     String idToken,
     SocialProviderType provider,
   ) onFetched;
+
+  final bool isSignUp;
 
   @override
   Widget build(BuildContext context) {
@@ -33,6 +37,7 @@ class SocialButtons extends StatelessWidget {
             width: double.infinity,
             child: SignInButton(
               Buttons.Facebook,
+              text: isSignUp ? tr('button.signup_with_facebook') : tr('button.login_with_facebook'),
               onPressed: () {
                 socialAuthService.getFacebookIdToken().then((value) {
                   if (value != null) onFetched(value, SocialProviderType.facebook);
@@ -48,6 +53,7 @@ class SocialButtons extends StatelessWidget {
             width: double.infinity,
             child: SignInButton(
               Buttons.Google,
+              text: isSignUp ? tr('button.signup_with_google') : tr('button.login_with_google'),
               onPressed: () {
                 socialAuthService.getGoogleIdToken().then((value) {
                   if (value != null) onFetched(value, SocialProviderType.google);
