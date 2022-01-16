294bb361cdf0b925bbf74f6dfe7407085a4c8cda
Thea Choem <theacheng.g6@gmail.com>
2022-01-16 10:19:39 +0700

    build: configured for release v1.1.7+16

---
+++ lib/services/storages/search_history_storage.dart
@@ -0,0 +1,6 @@
+import 'package:cambodia_geography/services/base_storages/list_preference_storage.dart';
+
+class SearchHistoryStorage extends ListPreferenceStorage {
+  @override
+  String get key => "SearchHistoryStoraged";
+}
